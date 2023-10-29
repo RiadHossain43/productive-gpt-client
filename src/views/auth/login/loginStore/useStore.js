b50f3e5e90060682f485caf222279a63f90ef646
RiadHossain43 <riadhossain7464@gmail.com>
2023-10-29 18:20:45 +0600

    updated chat

--- src/views/auth/login/loginStore/useStore.js
+++ src/views/auth/login/loginStore/useStore.js
@@ -28,7 +28,7 @@ export default function useStore(config) {
         refreshToken: data.details?.refreshToken,
       });
       navigate(
-        `/accounts/auth-success?user_id=${data.details.user._id}&redirect=/chat`
+        `/accounts/auth-success?user_id=${data.details.user._id}&redirect=/magic-box`
       );
       _dispatch({
         [USER_ACTIONS.LOGIN]: {
